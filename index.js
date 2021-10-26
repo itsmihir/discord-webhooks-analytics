289ade5e87991abc0b94a392a984b2273de2e177
itsmihir <inmihir11@gmail.com>
2021-10-26 18:02:04 +0530

    ip fetched

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get("/portfolio", async (req, res) => {
         ip = ip.substr(7);
     }
     console.error(ip);
+    console.log(ip);
     const details = await axios.get(`http://ipwhois.app/json/${ip}`);
     const currentTime = Date();
     const data = {
